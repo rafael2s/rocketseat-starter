4d2443062190942dd1becfc01d0c4d90b09a0294
Rafael Souza <rafael_popo@hotmail.com>
2020-04-23 10:42:28 -0300

    Conceitos JS-ESC6 iniciado

---
+++ JavaScript-ES6/bundle.js
@@ -0,0 +1,19 @@
+// Operações em array
+//     const arr = [1, 3, 4, 5, 8, 9];
+//     const newArr = arr.map(function(item, index) {
+//         return item + index;
+//     });
+//     console.log(newArr);
+//     const soma = arr.reduce(function(total, next) { 
+//         return total + next; 
+//     });
+//     console.log(soma); 
+//     const filter = arr.filter(function(item) {
+//         return item % 2 === 0;
+//     });
+//     console.log(filter);
+//     const find = arr.find(function(item) {
+//         return item === 4;
+//     });
+//     console.log(find);
+"use strict";

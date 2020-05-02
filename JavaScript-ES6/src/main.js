366305863600fecee4948eb6cd91d95fe2f3dcf6
Rafael Souza <rafael_popo@hotmail.com>
2020-05-02 20:59:52 -0300

    Modulo 3 JSES6 finalizado

--- JavaScript-ES6/src/main.js
+++ JavaScript-ES6/src/main.js
@@ -1,1 +1,32 @@
-alert('Testando3');
+// const minhaPromise = () => new Promise ((resolve, reject) => {
+//     setTimeout(() => { resolve('OK') }, 2000);
+// });
+
+// // async function executaPromise() {
+// //     console.log(await minhaPromise());
+// //     console.log(await minhaPromise());
+// //     console.log(await minhaPromise());
+// // }
+
+// const executaPromise = async () => {
+//     console.log(await minhaPromise());
+//     console.log(await minhaPromise());
+//     console.log(await minhaPromise());
+// };
+
+// executaPromise();
+
+import axios from 'axios';
+
+class Api {
+    static async getUserInfo(username) {
+        try {
+            const response = await axios.get(`https://api.github.com/users/${username}`);
+            console.log(response);
+        } catch(err) {
+            console.warn('Erro na API');
+        }
+    }
+};
+
+Api.getUserInfo('rafael2');
